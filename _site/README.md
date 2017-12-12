316df31a8b5f303048196c5d26a3ecc64e451501
cr0hn <cr0hn@cr0hn.com>
2017-12-12 16:57:32 +0100

    Modification in site

--- _site/README.md
+++
@@ -1,5 +0,0 @@
-This respository contains the source code of OWASP Madrid web site
-
-Web site | http://owaspmadrid.org
----- | ----------------------------------------------
-Web en github | https://owaspmadrid.github.io/
--- build-site.sh
+++ build-site.sh
@@ -1,5 +1,24 @@
+# Ensure correct branch
+git checkout source
+
+# Build site
+jekyll build
+
+# Build static stie
+git add *
 git commit -am "Modification in site"
-git checkout master _site/* .
-git commit -am "New static site"
-#git push
 git checkout master
+git checkout source -- _site
+
+mv _site/* .
+rm -rf _site/
+
+git add *
+git commit -am "New static site"
+
+# Back to source branch
+git checkout source
+
+# Upload changes
+git push --all
+
