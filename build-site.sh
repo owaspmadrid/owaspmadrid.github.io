8c3e9c2e1f2c168c99720d48ecf98eb79c168e37
cr0hn <cr0hn@cr0hn.com>
2017-12-12 16:54:14 +0100

    Revert "fixed branches"
    This reverts commit 9fcba477b6f5d763cf4584427d5ec09424eb45f4.

--- build-site.sh
+++ build-site.sh
@@ -1,7 +1,6 @@
-git checkout source
-git commit -am "Modification in site"
 git checkout master
-git checkout source _site/* .
+git commit -am "Modification in site"
+git checkout master _site/* .
 git commit -am "New static site"
 git push --all
-git checkout source
+git checkout master
